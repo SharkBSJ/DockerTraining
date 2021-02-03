8abc79faeead283c961614d067a34f9264f30d6c
SharkBSJ <baesungjin1996@gmail.com>
2021-02-03 21:36:22 +0900

    First Commit

---
+++ DockerTraining/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DockerTraining project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DockerTraining.settings')
+
+application = get_wsgi_application()
